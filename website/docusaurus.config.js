f6e049e232f2bd58a0f61a0eb05773179c5acc5d
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2024-01-04 14:33:11 +0000

    fixing opengraph link on website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/opengraph.jpg',
+      image: 'img/opengraph.png',
       colorMode: {
           defaultMode: 'light'
       },
