944866f8e6940323075ed05bd92935d06550b5ba
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2024-01-09 07:49:56 +0000

    dynamodb

--- packages/cloudcatalog-cli/src/resources/index.ts
+++ packages/cloudcatalog-cli/src/resources/index.ts
@@ -1,8 +1,9 @@
 import * as lambda from "./lambda";
 import * as states from "./states";
 import * as sqs from "./sqs";
+import * as dynamodb from "./dynamodb";
 
-type ResourceData = lambda.Data | states.Data | sqs.Data;
+type ResourceData = lambda.Data | states.Data | sqs.Data | dynamodb.Data;
 
 interface Resource {
   getData: (arn: string) => Promise<any>;
@@ -14,4 +15,5 @@ export default {
   lambda,
   states,
   sqs,
+  dynamodb
 } as Record<string, Resource>;
